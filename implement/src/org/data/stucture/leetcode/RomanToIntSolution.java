478b28eb28dbf26b01ed822c6fbb97e0a20fef21
SF <sf_work@foxmail.com>
2020-03-09 21:42:52 +0800

    leetcode  13.罗马数字转整数

---
+++ implement/src/org/data/stucture/leetcode/RomanToIntSolution.java
@@ -0,0 +1,54 @@
+package org.data.stucture.leetcode;
+
+public class RomanToIntSolution {
+    public static RomanToIntSolution Instance = new RomanToIntSolution();
+
+    public int romanToInt(String s){
+        if (s == null || s.length() < 1) return 0;
+        int headNum = getIntByChar(s.charAt(0));
+        int result = 0;
+        for (int i = 1; i < s.length(); i++) {
+            int next = getIntByChar(s.charAt(i));
+            if (headNum < next){
+                result -= headNum;
+            } else {
+                result += headNum;
+            }
+
+            headNum = next;
+        }
+
+        result += headNum;
+
+
+        return result;
+    }
+
+    public static int getIntByChar(char s){
+        switch (s){
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Instance.romanToInt("III"));
+        System.out.println(Instance.romanToInt("IIV"));
+        System.out.println(Instance.romanToInt("MCMXCIV"));
+    }
+}
